55c2ee8f188247ec81f39b5f528704428dfcb472
Mattias Carlsson <mattias.carlsson01@gmail.com>
2020-05-12 20:07:30 +0200

    feat(policealert-populator): Replaced ternary with optional chain

--- src/main/java/se/disabledsecurity/policealert/populator/entity/converters/AbstractEnumConverter.java
+++ src/main/java/se/disabledsecurity/policealert/populator/entity/converters/AbstractEnumConverter.java
@@ -2,6 +2,7 @@ package se.disabledsecurity.policealert.populator.entity.converters;
 
 import javax.persistence.AttributeConverter;
 import java.util.Arrays;
+import java.util.Optional;
 
 public abstract class AbstractEnumConverter <T extends Enum<T> & PersistableEnum<E>, E> implements AttributeConverter<T, E> {
     private final Class<T> clazz;
@@ -12,7 +13,9 @@ public abstract class AbstractEnumConverter <T extends Enum<T> & PersistableEnum
 
     @Override
     public E convertToDatabaseColumn(T attribute) {
-        return attribute != null ? attribute.getValue() : null;
+        return Optional.ofNullable(attribute)
+                .map(PersistableEnum::getValue)
+                .orElse(null);
     }
 
     @Override
